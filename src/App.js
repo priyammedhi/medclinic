b239872db6ecb126f94b195d82e8abca35a117ae
priyammedhi <75823826+priyammedhi@users.noreply.github.com>
2024-09-20 16:00:26 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Use HashRouter
 import Navbar from './components/Navbar';
 import Home from './screens/Home';
 import Footer from './components/Footer';
@@ -11,18 +11,15 @@ import Selfscrolltop from './components/Selfscrolltop';
 
 function App() {
   return (
-    <Router>
+    <Router> {/* Use HashRouter instead of BrowserRouter */}
       <div className="App">
         <Selfscrolltop />
         <Navbar />
-        <Home/>
-        {/* Define Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Servicepage />} />
           <Route path="/about" element={<Aboutus />} />
         </Routes>
-
         <Footer />
       </div>
     </Router>
