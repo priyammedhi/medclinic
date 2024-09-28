984b6f3eb2bc379f74d8b44c09f6b91702d4342d
priyammedhi <75823826+priyammedhi@users.noreply.github.com>
2024-09-28 15:37:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Selfscrolltop />
         <Whatsappicon />
         <Navbar />
+        <Home/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Servicepage />} />
